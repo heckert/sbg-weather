7462eed7704f322ff8e42a0ff557471c6794f945
Heinz Eckert <heinz_eckert@hotmail.com>
2021-08-15 13:07:37 +0200

    SplitScaler class

--- src/helpers.py
+++ src/helpers.py
@@ -1,8 +1,11 @@
-import os
-import numpy as np
-import pandas as pd
+import datetime
 import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
 import tensorflow as tf
+
+from sklearn.preprocessing import StandardScaler
 
 def get_filenames_in_directory(path):
     f = []
@@ -11,6 +14,88 @@ def get_filenames_in_directory(path):
         break
 
     return f 
+
+class SplitScaler:
+    '''
+    Split df into train, validation & test data,
+    fit individual scalers for each location on train data,
+    and transform all dataframes accordingly
+    '''
+    def __init__(self, df:pd.DataFrame, train_val_ratios:tuple):
+        self.df = df
+        self.train_share, self.val_share = train_val_ratios
+        self.unique_locations = df.index.get_level_values(0).unique()
+        self._split_data()
+        self._scale()
+
+    def _split_data(self):
+        # create slices for training, validation & testing
+        self.min_date = self.df.index.get_level_values(1).min().strftime('%Y-%m-%d')
+        self.max_date = self.df.index.get_level_values(1).max().strftime('%Y-%m-%d')
+
+        d_range = pd.date_range(self.min_date, self.max_date, freq='D')
+        # days need to be string rather than datetime for 
+        # dataframe sclicing to work as expected
+        d_range = d_range.map(lambda x: datetime.datetime.strftime(x, '%Y-%m-%d'))
+        n_dates = len(d_range)
+
+        train_range = d_range[:int(n_dates*self.train_share)]
+        val_range = d_range[int(n_dates*self.train_share):int(n_dates*(self.train_share+self.val_share))]
+        test_range = d_range[int(n_dates*(self.train_share+self.val_share)):]
+
+        self.train_slice = slice(train_range[0], train_range[-1])
+        self.val_slice = slice(val_range[0], val_range[-1])
+        self.test_slice = slice(test_range[0], test_range[-1])
+
+        train_unsc = self.df.loc[pd.IndexSlice[:,self.train_slice],:].copy()
+        val_unsc = self.df.loc[pd.IndexSlice[:,self.val_slice],:].copy()
+        test_unsc = self.df.loc[pd.IndexSlice[:,self.test_slice],:].copy()
+
+        self.split_data_unsc = train_unsc, val_unsc, test_unsc
+
+    def plot(self):
+        fig, ax = plt.subplots(figsize=(10,2))
+        d_range = pd.date_range('2016-01-01', '2019-12-31')
+
+        data = self.df.loc['Zell am See - Eishalle']
+
+        data.loc[self.train_slice, 'Lufttemperatur [GradC]'].plot(ax=ax, color='tab:blue', label='Train')
+        data.loc[self.val_slice, 'Lufttemperatur [GradC]'].plot(ax=ax, color='tab:orange', label='Validation')
+        data.loc[self.test_slice, 'Lufttemperatur [GradC]'].plot(ax=ax, color='tab:green', label='Test')
+        ax.legend()
+        ax.set_title('Train, Validation & Test Set')
+        plt.show()
+
+    def _scale(self):
+        self.scalers = {}
+        ds_types = ('train', 'val', 'test')
+        containers = ([], [], [])  # empty arrays to store scaled dfs per location
+
+        for ds_type, df_unsc, container in zip(ds_types, self.split_data_unsc, containers):
+            for location in self.unique_locations:
+                # get data per location 
+                df_loc = df_unsc.loc[location]
+                times = df_loc.index
+
+                # fit scaler only on training data
+                if ds_type == 'train':
+                        scaler = StandardScaler()
+                        scaler = scaler.fit(df_loc)
+
+                        # Keep scaler per location
+                        self.scalers[location] = scaler
+                    
+                scaled_data = scaler.transform(df_loc)
+
+                # Create MultiIndex from location and array of measurement times
+                index = pd.MultiIndex.from_product([[location], times], names=['Messort', 'Zeitpunkt'])
+                df_loc_sc = pd.DataFrame(scaled_data, index=index, columns=df_loc.columns)
+
+                container.append(df_loc_sc)
+
+            concat_locations_df = pd.concat(container)
+            self.__setattr__(ds_type, concat_locations_df)
+
 
 class WindowGenerator:
     '''
@@ -120,12 +205,44 @@ class WindowGenerator:
                 .map(self._split_xy),
             num_parallel_calls=tf.data.AUTOTUNE)
 
-        # Shuffle, batch and prefetch the data
+        # Shuffle before batching
         dataset = dataset.shuffle(300) \
             .batch(self.batch_size) \
             .prefetch(tf.data.AUTOTUNE)
 
         return dataset
+
+    def plot(self, model=None, plot_col='Lufttemperatur [GradC]', max_subplots=3):
+        (inputs, embeddings), labels = self.example
+        plt.figure(figsize=(12, 8))
+        plot_col_index = self.column_indices[plot_col]
+        max_n = min(max_subplots, len(inputs))
+        for n in range(max_n):
+            plt.subplot(max_n, 1, n+1)
+            plt.ylabel(f'Temp [normed]')
+            plt.plot(self.input_indices, inputs[n, :, plot_col_index],
+                    label='Inputs', marker='.', zorder=-10)
+
+            if self.label_columns:
+                label_col_index = self.label_columns_indices.get(plot_col, None)
+            else:
+                label_col_index = plot_col_index
+
+            if label_col_index is None:
+                continue
+
+            plt.scatter(self.label_indices, labels[n, :, label_col_index],
+                        edgecolors='k', label='Labels', c='#2ca02c', s=64)
+            if model is not None:
+                predictions = model((inputs, embeddings))
+                plt.scatter(self.label_indices, predictions[n, :, label_col_index],
+                            marker='X', edgecolors='k', label='Predictions',
+                            c='#ff7f0e', s=64)
+
+            if n == 0:
+                plt.legend()
+
+        plt.xlabel('Zeit [h]')
 
     @property
     def train(self):
@@ -139,5 +256,13 @@ class WindowGenerator:
     def test(self):
         return self.make_dataset(self.test_dir)
 
-
-  
+    @property
+    def example(self):
+        """Get and cache an example batch of `inputs, labels` for plotting."""
+        #result = getattr(self, '_example', None)
+        #if result is None:
+            # No example batch was found, so get one from the `.test` dataset
+        result = next(iter(self.test))
+        # And cache it for next time
+        # self._example = result
+        return result
